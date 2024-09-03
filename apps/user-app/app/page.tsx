858357c4a33c4cb2034022a87d01f8a326c72d40
viveak910 <1602-23-737-191@vce.ac.in>
2024-09-03 23:10:27 +0530

    turborepo setup done

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="text-2xl">
+      hi there
+    </div>
+   
+  );
+}
