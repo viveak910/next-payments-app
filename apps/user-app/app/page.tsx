9d83801e19f0350f4a82f9ff51be0167201ee773
viveak910 <1602-23-737-191@vce.ac.in>
2024-09-04 20:39:25 +0530

    some config changes:ignorable

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/client";
 
 export default function Home() {
   return (
